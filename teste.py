b49328cf1efdcfbebb770ae59a34b157fd07f57b
Pedrohaf <pedro_22hferreira@live.com>
2022-07-07 13:40:09 -0300

    Commit adicionado comentarios

--- teste.py
+++ teste.py
@@ -1,5 +1,7 @@
-import time
+# Importando Biblioteca
+import time                                     
 
+# Usando for para fazer o lace de repetição e ultilizando o a funçao time.sleep(1) para dar 1 seg  para resposta o sistema
 print ('-------------- contagem Regressiva para detonção------------------')
 for i in range (10,0,-1):
     print(i)
