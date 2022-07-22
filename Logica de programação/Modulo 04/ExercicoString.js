88ec4bce9acc9ef024e1b8629cd564ef26f33854
Pedrohaf <pedro_22hferreira@live.com>
2022-07-22 12:44:26 -0300

    commit do exercico de string

---
+++ Logica de programação/Modulo 04/ExercicoString.js
@@ -0,0 +1,17 @@
+
+// transformando os as palavras me maiusculas
+let nome = 'pedro Henrique'
+
+console.log(nome.toUpperCase)
+
+// ------------------------------------------------------------
+// convertendo para inteiro
+
+let cep = '52090345'
+parseInt(cep)
+console.log(typeof cep)
+
+// -------------------------------------------------------------
+// mostrando a quantidade de caracter
+console.log(nome.length)
+
