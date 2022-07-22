ffc765a4e2d2b480a7942bdf66561acb98d2eb54
Pedrohaf <pedro_22hferreira@live.com>
2022-07-22 14:12:27 -0300

    Commit exercicio de Serialização

---
+++ ExercicioSerialização/src/Pessoa.java
@@ -0,0 +1,88 @@
+import java.io.Serializable;
+
+public class Pessoa implements Serializable {
+    private int id;
+    private String nome;
+    private double salario;
+    private String senha;
+    private String endereco;
+
+    public Pessoa(int id, String nome, double salario, String senha, String endereco){
+        this.setId(id);
+        this.setNome(nome);
+        this.setSalario(salario);
+        this.setSenha(senha);
+        this.setEndereco(endereco);
+    }
+    /**
+     * @return int return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return double return the salario
+     */
+    public double getSalario() {
+        return salario;
+    }
+
+    /**
+     * @param salario the salario to set
+     */
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    /**
+     * @return String return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+    /**
+     * @return String return the endereco
+     */
+    public String getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * @param endereco the endereco to set
+     */
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+}
