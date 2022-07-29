765c4493c03f30873b8000243cbcc90b33a53ac4
Pedrohaf <pedro_22hferreira@live.com>
2022-07-29 19:59:31 -0300

    Commit do exercicio 02

---
+++ JavaScript/Modulo 03/exercicio02.js
@@ -0,0 +1,16 @@
+function teste (){
+    console.log('Ola mundo')
+}
+
+var x = 'Ola mundo '
+
+function teste2 (x) {
+    return x
+}
+
+const arrowFucinto = (x) => x;
+
+teste()
+console.log(teste2(x))
+console.log(arrowFucinto(x))
+
