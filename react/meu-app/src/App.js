7aaedb502236417b95019e85ff12152bc1c1e754
Pedrohaf <pedro_22hferreira@live.com>
2022-09-21 18:58:14 -0300

    commit exercicio 02

--- react/meu-app/src/App.js
+++ react/meu-app/src/App.js
@@ -5,21 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Edit <code>src/App.js</code> teste teste teste
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p>Pedro Henrique Almeida Ferreira</p>
       </header>
     </div>
   );
