0aca989851a362d0f6a54747883f4c7ec30cedc0
Pedrohaf <pedro_22hferreira@live.com>
2022-12-22 15:47:03 -0300

    soap java

---
+++ webSservice/Modulo 2/atv02.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/EjbWebService.java to edit this template
+ */
+package Pacote1;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Hertz Informatica
+ */
+@WebService(serviceName = "soma")
+@Stateless()
+public class soma {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
